ebd0112551f33aa04060f161f96b36d1bfd14c2c
jinyong <jenokim1105@gmail.com>
2019-06-27 19:07:23 +0700

    Ended all the instructions

--- blog/models.py
+++ blog/models.py
@@ -5,3 +5,13 @@ class Blog(models.Model):
     pub_date = models.DateTimeField()
     body = models.TextField()
     image = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        # inside classes, you gotta path self to make a function
+        return self.body[:200] + "..."
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
